89fb1b8a7acba37608e73371b3fd069334fca183
Florian Wartner <f.wartner@icloud.com>
2018-04-13 09:24:01 +0200

    Changes

--- resources/assets/js/components/Conferences.vue
+++ resources/assets/js/components/Conferences.vue
@@ -1,46 +1,51 @@
 <template>
     <div>
-        <div class="row">
-            <div class="col-4" v-for="conference in conferences">
-                <div class="card">
-                    <!--<img v-bind:src="conference.header_image" v-bind:alt="conference.title" class="card-img-top">-->
-                    <div class="card-body">
-                        <h5 class="card-title">
-                            <a v-bind:href="'/c/' + conference.id">{{ conference.title }}</a>
-                        </h5>
+        <div v-if="conferences.length">
+            <div class="row">
+                <div class="col-4" v-for="conference in conferences">
+                    <div class="card">
+                        <!--<img v-bind:src="conference.header_image" v-bind:alt="conference.title" class="card-img-top">-->
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                <a v-bind:href="'/c/' + conference.id">{{ conference.title }}</a>
+                            </h5>
 
-                        <p class="card-text">{{ truncate(conference.description, 120) }}</p>
+                            <p class="card-text">{{ truncate(conference.description, 120) }}</p>
+                        </div>
                     </div>
+                    <br>
                 </div>
-                <br>
             </div>
-        </div>
-        <div class="row justify-content-md-center" v-show="withPagination">
-            <div class="col align-self-center">
-                <nav aria-label="pagination">
-                    <ul class="pagination">
-                        <li class="page-item" v-bind:class="{ disabled: !links.prev }">
+            <div class="row justify-content-md-center" v-show="withPagination">
+                <div class="col align-self-center">
+                    <nav aria-label="pagination">
+                        <ul class="pagination">
+                            <li class="page-item" v-bind:class="{ disabled: !links.prev }">
                             <span class="page-link" v-on:click="getConferences(links.prev)">
                                 <span aria-hidden="true">&laquo;</span>
                                 <span class="sr-only">Previous</span>
                             </span>
-                        </li>
+                            </li>
 
-                        <li class="page-item" v-bind:class="{ active: currentPage === page }" v-for="page in pages">
-                            <a class="page-link" v-on:click="getConferences('api/conferences?page=' + page)">{{ page }}</a>
-                            <span class="sr-only">(current)</span>
-                        </li>
+                            <li class="page-item" v-bind:class="{ active: currentPage === page }" v-for="page in pages">
+                                <a class="page-link" v-on:click="getConferences('api/conferences?page=' + page)">{{ page }}</a>
+                                <span class="sr-only">(current)</span>
+                            </li>
 
-                        <li class="page-item" v-bind:class="{ disabled: !links.next }">
-                            <a class="page-link" v-on:click="getConferences(links.next)">
-                                <span aria-hidden="true">&raquo;</span>
-                                <span class="sr-only">Next</span>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
+                            <li class="page-item" v-bind:class="{ disabled: !links.next }">
+                                <a class="page-link" v-on:click="getConferences(links.next)">
+                                    <span aria-hidden="true">&raquo;</span>
+                                    <span class="sr-only">Next</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
         </div>
+        <h2 class="text-center text-muted" v-else>
+            There are no conferences at this time. How about <a href="/c/create">create</a> one?
+        </h2>
     </div>
 </template>
 
@@ -64,12 +69,17 @@
 
         methods: {
             getConferences: function(link) {
+                let perPage = this.per_page;
+                if (typeof perPage === 'undefined') {
+                    perPage = '12';
+                }
+
                 if (typeof link === 'undefined') {
                     link = 'api/conferences?page=1';
                 }
 
                 let self = this;
-                axios.get(link + '&perPage=' + this.per_page)
+                axios.get(link + '&perPage=' + perPage)
                     .then(function (response) {
                         self.conferences = response.data.data;
                         self.links = response.data.links;
